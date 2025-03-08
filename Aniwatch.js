76bc388289173a5db4231984a03d61570efa53e6
SwapnilEzio04 <swapnilauditore123@gmail.com>
2025-03-08 16:54:50 +0530

    Add files via upload

--- Aniwatch.js
+++ Aniwatch.js
@@ -1,48 +1,42 @@
-
 let slideIndex = 0;
 
 function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+  let slides = document.getElementsByClassName("slide");
 
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000);// Change image every 3 seconds
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 // Manual navigation
 function changeSlide(n) {
-    slideIndex += n - 1;
-    showSlides();
+  slideIndex += n - 1;
+  showSlides();
 }
 showSlides();
 
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("close-btn");
+const overlay = document.getElementById("overlay");
+hamburgerBtn.addEventListener("click", function () {
+  sidebar.classList.add("open");
+  overlay.style.display = "block";
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("open");
+  overlay.style.display = "none";
+});
+overlay.addEventListener("click", function () {
+  sidebar.classList.remove("open");
+  overlay.style.display = "none";
+});
 
-    const hamburgerBtn = document.getElementById("hamburger-btn");
-    const sidebar = document.getElementById("sidebar");
-    const closeBtn = document.getElementById("close-btn");
-    const overlay = document.getElementById("overlay");
-    hamburgerBtn.addEventListener("click",function(){
-        sidebar.classList.add("open");
-        overlay.style.display = "block";
-    });   
-    closeBtn.addEventListener("click",function(){
-        sidebar.classList.remove("open");
-        overlay.style.display = ("none");
-    });
-    overlay.addEventListener("click",function(){
-        sidebar.classList.remove("open");
-        overlay.style.display = ("none");
-    })
-
-
-
-
-
-
-    
